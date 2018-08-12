30ddcbe781aa43e63c6921fde38a7c6c652b251d
BackRaw <backraw@gmx.de>
2018-08-12 05:04:43 +0200

    Add project skeleton
    Signed-off-by: BackRaw <backraw@gmx.de>

---
+++ addons/source-python/plugins/flashfun/info.py
@@ -0,0 +1,16 @@
+# ../flashfun/info.py
+
+"""Provides/stores information about the plugin."""
+
+# =============================================================================
+# >> IMPORTS
+# =============================================================================
+# Source.Python Imports
+#   Plugins
+from plugins.manager import plugin_manager
+
+
+# =============================================================================
+# >> PLUGIN INFO
+# =============================================================================
+info = plugin_manager.get_plugin_info(__name__)
